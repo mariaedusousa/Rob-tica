4ac480b10650baa0070fb20e3044e3d8bb8450c7
mariaedusousa <mariaeduardasousasantoss071@gmail.com>
2025-07-03 08:09:01 -0300

    Questão22.cpp

---
+++ Questão22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   float salario, resultado;
+   const float bonus = 15.0/100.0;
+   
+   cout<<"Informe o valor do salario: "<<endl;
+   cin>>salario;
+   
+   resultado = salario + (salario * bonus );
+   
+   cout<<"O valor do salario com o bunos sera de "<<resultado<<endl;
+    return 0;
+}
